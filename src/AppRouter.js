a6ef78422f92fe386e1046073ed69eed67c1594b
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2022-02-03 14:28:46 +0100

    deleted routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import AllPokemon from "./components/AllPokemon";
-import Pokemon from "./components/Pokemon";
-import PokemonInfo from "./components/PokemonInfo";
 import Home from "./pages/Home";
 import PokeVsPoke from "./pages/PokeVsPoke";
 import BattleField from "./pages/BattleField";
@@ -15,9 +12,6 @@ const AppRouter = () => {
         <Route path="/battle" element={<BattleField />}></Route>
 
         {/*these are routes to get data from API */}
-        <Route exact path="/pokemon" element={<AllPokemon />}></Route>
-        <Route path="/pokemon/:id" element={<Pokemon />}></Route>
-        <Route path="/pokemon/:id/:info" element={<PokemonInfo />}></Route>
       </Routes>
     </>
   );
